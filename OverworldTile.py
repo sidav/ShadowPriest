848fb2d258ee24dd3f9afa47470779f553638ff8
sidav <sidav94@Gmail.com>
2017-10-23 17:18:42 +0300

    Trying coding from smartphone.

--- OverworldTile.py
+++ OverworldTile.py
@@ -2,6 +2,7 @@
 #represents one tile of the global map.
 class OverworldTile:
     _appearance = '?'
+    _color = null;
 
     def __init__(self, appearance):
         self._appearance = appearance
