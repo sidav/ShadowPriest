7405fd7cf21bae9998aa258d0913b191ec16615e
sidav <sidav94@gmail.com>
2017-11-21 00:10:26 +0300

    Test-debug.

--- _TESTDEBUG.py
+++ _TESTDEBUG.py
@@ -1,7 +1,8 @@
 import Routines.TdlConsoleWrapper as CW
 from Level_Routines import LevelBuilder as LevelBld
+from Overworld_Routines import Overworld as OWD
 
 #Following file is just a testing ground for any shit possible. It should not interfere with any other logic and should cause no problems when deleted.
 
 def makeSomeTestCrap():
-    crap = LevelBld._getBasicLevel(79, 24)
+    crap = OWD()
