09fa791e2194fc19dbb0f7aef50f47057f585e7d
sidav <sidav94@gmail.com>
2017-12-30 22:16:06 +0300

    Started working on levels.

--- Level_Routines/LevelTile.py
+++ Level_Routines/LevelTile.py
@@ -5,11 +5,18 @@ class Tile:
 
     _appearance = '?'
     __color = 0xFFFFFF #fuck
-    _visionObstructing = True
+    _vision_obstructing = True
     _passable = False
 
     def __init__(self, appearance, passable, visionObstructing):
-        #global _appearance, _passable, _visionObstructing
+        #global _appearance, _passable, _vision_obstructing
         self._appearance = appearance
         self._passable = passable
-        self._visionObstructing = visionObstructing
+        self._vision_obstructing = visionObstructing
+        
+    
+    def get_passable(self):
+        return self._passable
+    
+    def get_vision_obstructing(self):
+        return self._vision_obstructing
