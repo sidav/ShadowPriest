16c80568182cb1634915e0e5508b66644ca438c9
sidav <sidav94@gmail.com>
2018-01-13 14:06:48 +0300

    WIP: "lookx == looky == 0" bug hotfix!

--- Level_Routines/Unit.py
+++ Level_Routines/Unit.py
@@ -4,15 +4,16 @@ import Routines.SidavRandom as random
 class Unit:
 
     _pos_x = _pos_y = 0
-    _look_x = _look_y = 1
+    _look_x = _look_y = 0
     _has_look_direction = True # <-- is the "looking thingy" neccessary to draw?
     _appearance = 'G'
 
     def __init__(self, posx, posy, appearance = 'G'):
         self._pos_x = posx
         self._pos_y = posy
-        self._look_x = random.rand(3) - 1
-        self._look_y = random.rand(3) - 1
+        while self._look_x == 0 and self._look_y == 0:
+            self._look_x = random.rand(3) - 1
+            self._look_y = random.rand(3) - 1
         pass
 
     def move_forward(self):
