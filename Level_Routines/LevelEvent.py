f07258e23778c9820d713692e311b90d7b1e1ff7
Vladislav Kovun <vkovun@phoenixit.ru>
2018-02-22 16:38:54 +0300

    WIP: started working on event system.

---
+++ Level_Routines/LevelEvent.py
@@ -0,0 +1,20 @@
+
+class Level_Event:
+
+    text = "empty event"
+
+    visual = True
+
+    acustic = True
+    hear_radius = 3
+
+    continious = False
+    expiration_turn = 0
+
+    def __init__(self, text, visual, hear_radius=0, expiration_turn = 0):
+        self.text = text
+        self.visual = visual
+        self.acustic = hear_radius > 0
+        self.hear_radius = hear_radius
+        self.continious = expiration_turn > 0
+        self.expiration_turn = expiration_turn
