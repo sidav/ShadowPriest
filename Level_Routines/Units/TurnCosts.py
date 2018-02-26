7be1ae8ce4a267d8c5ce2665190e0e2a3b044230
sidav <sidav94@gmail.com>
2018-02-26 19:54:27 +0300

    Added the placeholder turns cost for dropping items from player inv.

--- Level_Routines/Units/TurnCosts.py
+++ Level_Routines/Units/TurnCosts.py
@@ -17,6 +17,8 @@ def cost_for(action, unit=None):
         return 10
     elif action == 'pick up':
         return 12
+    elif action == 'drop item':
+        return 10
 
     else:
         LOG.append_error_message('Time cost for unknown action "{0}" requested.'.format(action))
