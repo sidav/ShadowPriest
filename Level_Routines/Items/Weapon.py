41518578c7dfca3478b4a62e70b600c27ce88491
sidav <sidav94@gmail.com>
2018-03-19 23:13:53 +0300

    Added stats to weapons.

--- Level_Routines/Items/Weapon.py
+++ Level_Routines/Items/Weapon.py
@@ -1,4 +1,13 @@
 from .Item import Item
 
+
 class Weapon(Item):
+    _base_damage = 1
+    _base_stab_damage = 25
+    _base_time = 1
+    _base_tohit = 50  # in percent
+    _min_str_to_use = 3
+    _two_handed = False
+    _damage_type = 0  # fuck knows what that mean.
+
     pass
