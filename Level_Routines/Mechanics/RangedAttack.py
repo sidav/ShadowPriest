5c3ad91c69dd84d726f1fe5645af2f55a0eeb830
sidav <sidav94@gmail.com>
2018-05-22 00:16:03 +0300

    Adds a placeholder for ranged attacks.

--- Level_Routines/Mechanics/RangedAttack.py
+++ Level_Routines/Mechanics/RangedAttack.py
@@ -1,14 +1,33 @@
 from ..Units.Unit import Unit
 from Message_Log import MessageLog as LOG
 from . import Damage as DMG
+from Routines import SidavLOS as LOS
 
 
 def try_to_shoot(attacker:Unit, victim:Unit):
-    if attacker.get_inventory().get_equipped_weapon() is None:
-        LOG.append_warning_message('trying to stab with no weapon')
-        return False
+    a_x, a_y = attacker.get_position()
+    v_x, v_y = victim.get_position()
     attacker_weapon = attacker.get_inventory().get_equipped_weapon()
+    if attacker_weapon is None or not attacker_weapon.is_of_type('RangedWeapon'):
+        LOG.append_warning_message('trying to shoot with no ranged weapon')
+        return False
+
+    if not LOS._straightLOSCheck(a_x, a_y, v_x, v_y): # TODO: maybe use fullLOSLineCheck()?
+        return False
+
+    # TODO: check for enough ammo in clip
+    # TODO: implement auto-firing guns
+
     attacker_stats = attacker.get_rpg_stats()
-    damage = calculate_stab_damage(attacker_weapon.get_base_stab_damage(), attacker_stats)
+    damage = calculate_shooting_damage(attacker_weapon, attacker_stats)
+    spend_ammo(attacker_weapon)
     DMG.do_damage_to_victim(damage, victim)
     return True
+
+
+def calculate_shooting_damage(weapon, stats):
+    return 10
+
+
+def spend_ammo(weapon):
+    return 10
--- Level_Routines/Units/ActorController_Detection.py
+++ Level_Routines/Units/ActorController_Detection.py
@@ -4,7 +4,7 @@ from Routines import SidavLOS as LOS
 # routine for checking NPC behaviour and something
 
 def is_unit_seeing_position(lvl, actor, px, py):
-    vis_map = lvl.get_opacity_map()
+    # vis_map = lvl.get_opacity_map()
     from_x, from_y = actor.get_position()
     fov_angle = actor.get_fov_angle()
     look_x, look_y = actor.get_look_direction()
@@ -12,6 +12,6 @@ def is_unit_seeing_position(lvl, actor, px, py):
         return False
     if not ALOS.is_point_in_sector(from_x, from_y, look_x, look_y, px, py, fov_angle):
         return False
-    if not LOS._straightLOSCheck(from_x, from_y, px, py):
+    if not LOS._straightLOSCheck(from_x, from_y, px, py):  #TODO: WARNING: can cause seeing through closed doors or opposite behaviour. TODO: check that!
         return False
     return True
