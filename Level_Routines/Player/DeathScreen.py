ffb3450289cfd95d893c4d86cfc6b66b8d8e68af
sidav <sidav94@gmail.com>
2018-06-16 23:49:58 +0300

    Heavily improves the gameover animation.

--- Level_Routines/Player/DeathScreen.py
+++ Level_Routines/Player/DeathScreen.py
@@ -1,28 +1,34 @@
 from Routines import TdlConsoleWrapper as CW, SidavRandom as RND
 from GLOBAL_DATA import Global_Constants as GC
 
-_death_text = [
-    " _  _  _____  __ __ // _____ _____    _____ _____  ___  _____ ",
-    " \\\\// ((   )) || ||    ||_// ||==     ||  ) ||==  ||=|| ||  ) ",
-    "  //   \\\\_//  \\\\_//    || \\\\ ||___  _ ||_// ||___ || || ||_// ",
-    "                                                              "
+_death_texts = [
+    [
+        "## ##  #####  ###  ### # ######  ######    ######  ######  ######   ###### ",
+        "## ## ##% ### ##%  ### % ##  ### ##%       #%  ### ##%     ##%  ### #%  ###",
+        " %%#% #%# %#% #%#  %#%   #%%%#%  #%%%:%    %#  %#% #%%%:%  #%#%#%#% %#  %#%",
+        "  %:  %%: %%% %%:  %%%   %%  %   %%:       %:  %%% %%:     %%:  %%% %:  %%%",
+        "  :    : ::    :.:: :    %:   :  : :: :    : :: :  : :: :  :%   ::  : :: : "
+    ],
+    [
+        " _  _  _____  __ __ // _____ _____    _____ _____  ___  _____ ",
+        " \\\\// ((   )) || ||    ||_// ||==     ||  ) ||==  ||=|| ||  ) ",
+        "  //   \\\\_//  \\\\_//    || \\\\ ||___    ||_// ||___ || || ||_// ",
+        "                                                              "
+    ],
+    [
+        'Yb  dP  dP"Yb  88   88  .o. 88""Yb 888888     8888b.  888888    db    8888b.',
+        ' YbdP  dP   Yb 88   88  dP  88__dP 88__        8I  Yb 88__     dPYb    8I  Yb',
+        '  8P   Yb   dP Y8   8P      88"Yb  88""        8I  dY 88      dP  Yb   8I  dY',
+        '  dP    YbodP  `YbodP`      88  Yb 888888     8888Y"  888888 dP""""Yb 8888Y"'
+    ],
+    [
+        ' %%  %%   %%%%   %%  %% %% %%%%%   %%%%%%        %%%%%   %%%%%%   %%%%   %%%%%  ',
+        '  %%%%   %%  %%  %%  %% %% %%  %%  %%            %%  %%  %%      %%  %%  %%  %% ',
+        '   %%    %%  %%  %%  %%  % %%%%%   %%%%          %%  %%  %%%%    %%%%%%  %%  %% ',
+        '   %%    %%  %%  %%  %%    %%  %%  %%            %%  %%  %%      %%  %%  %%  %% ',
+        '   %%     %%%%    %%%%     %%  %%  %%%%%%        %%%%%   %%%%%%  %%  %%  %%%%%  '
+    ]
 ]
-
-# _death_text = [
-#     'Yb  dP  dP"Yb  88   88  .o. 88""Yb 888888     8888b.  888888    db    8888b.',
-#     ' YbdP  dP   Yb 88   88, dP  88__dP 88__        8I  Yb 88__     dPYb    8I  Yb',
-#     '  8P   Yb   dP Y8   8P      88"Yb  88""        8I  dY 88      dP  Yb   8I  dY',
-#     '  dP    YbodP  `YbodP`      88  Yb 888888     8888Y"  888888 dP""""Yb 8888Y"'
-# ]
-#
-# _death_text = [
-#     ' %%  %%   %%%%   %%  %% %% %%%%%   %%%%%%         %%%%%   %%%%%%   %%%%   %%%%%  ',
-#     '  %%%%   %%  %%  %%  %% %% %%  %%  %%             %%  %%  %%      %%  %%  %%  %% ',
-#     '   %%    %%  %%  %%  %%  % %%%%%   %%%%           %%  %%  %%%%    %%%%%%  %%  %% ',
-#     '   %%    %%  %%  %%  %%    %%  %%  %%             %%  %%  %%      %%  %%  %%  %% ',
-#     '   %%     %%%%    %%%%     %%  %%  %%%%%%         %%%%%   %%%%%%  %%  %%  %%%%%  '
-# ]
-
 def _determine_x_position(arr):
     ww = GC.CONSOLE_WIDTH
     if isinstance(arr, list):
@@ -39,45 +45,57 @@ def _determine_y_position(arr):
     return int((wh-pich)/3)
 
 
-def _draw_death_text():
-    # CW.setForegroundColor(196, 0, 0)
-    xpos = _determine_x_position(_death_text)
-    ypos = _determine_y_position(_death_text)
-    # CW.drawCharArrayAtPosition(_death_text, x, y, True)
-    # for i in range(len(_death_text)):
-    #     CW.putString(_death_text[i], x, i + y)
-    for y in range(len(_death_text)):
-        for x in range(len(_death_text[y])):
-            curr_char = list(_death_text[y])[x]
-            if curr_char != ' ':
-                CW.putChar(curr_char, x + xpos, y + ypos)
+def finalize_pic(pic):
+    final = []
+    for i in range(len(pic)):
+        final.append(list(pic[i]))
+
+    for i in range(len(final)):
+        for j in range(len(final[i])):
+            print("{}, {}".format(len(final), len(final[i])))
+            if final[i][j] == '#':
+                final[i][j] = chr(219)
+            elif final[i][j] == '%':
+                final[i][j] = chr(178)
+            elif final[i][j] == ':':
+                final[i][j] = chr(176)
+    return final
+
+def get_char_for_sparkle(death_text, fade_in, x = 0, y = 0):
+    blood_sparkles = [';', '!', '*', '%', '&', '?', '_', '\\', '/', '|', '(', ')', '$', '@', '~', '#', chr(177)]
+    if fade_in:
+        # curr_char = blood_sparkles[RND.rand(len(blood_sparkles))]
+        rand = RND.rand(255)
+        if 64 < rand < 91:
+            rand = RND.rand(255)
+        curr_char = chr(rand)
+        return curr_char
+    else:
+        xpos = _determine_x_position(death_text)
+        ypos = _determine_y_position(death_text)
+        if x - xpos < 0 or x - xpos >= len(death_text[0]) or y - ypos < 0 or y -ypos >= len(death_text):
+            # print('OUT OF BOUNDS, x{} xpos{} coord{}'.format(x, xpos, x-xpos))
+            return ' '
+        # print('IN BOUNDS, x{} xpos{} coord{}'.format(x, xpos, x - xpos))
+        curr_char = list(death_text[y-ypos])[x-xpos]
+        return curr_char
+
 
 
 def show_death_screen(player):
+    blood_per_frame = 50
+
+    _death_texts[0] = finalize_pic(_death_texts[0])
+    chosen_text = _death_texts[RND.rand(len(_death_texts))]
+
     bottom_margin = GC.LOG_HEIGHT + 2 # the 2 is the UI height
     w = GC.CONSOLE_WIDTH
     h = GC.CONSOLE_HEIGHT - bottom_margin
-    bloody_pixels_on_screen = [[' '] * h for _ in range(w)]
-    blood_sparkles = [',', ';', '!', '*', '`', '%', '&', '?', chr(177)]
+    bloody_pixels_on_screen = [[False] * h for _ in range(w)]
     total_shown_pixels = 0
     sparkles_color = (128, 0, 0)
-    blood_per_frame = 50
 
-    # FIRST: fade in the YOURE DEAD label.
-    text_r = 0
-    text_g = 0
-    text_b = 0
-    # while (text_r < 200):
-    #     CW.setForegroundColor(text_r, text_g, text_b)
-    #     draw_death_text()
-    #     text_r += 16
-    #     text_g += 1
-    #     text_b += 1
-    #     CW.flushConsole()
-    #     import time
-    #     time.sleep(0.1)
-
-    # SECOND: draw the blood around
+    # FIRST: fill the screen with blood.
     while total_shown_pixels < w * h:
         # draw blood
         for _ in range(blood_per_frame):
@@ -86,21 +104,21 @@ def show_death_screen(player):
             number = RND.rand(w * h)
             coord_x, coord_y = number % w, number // w
 
-            while bloody_pixels_on_screen[coord_x][coord_y] != ' ':
+            while bloody_pixels_on_screen[coord_x][coord_y]:
                 number += 1
                 if number >= w*h:
                     number = 0
                 coord_x, coord_y = number % w, number // w
 
             CW.setForegroundColor(sparkles_color)
-            sparkle = blood_sparkles[RND.rand(len(blood_sparkles))] # chr(RND.rand(256))
+            sparkle = get_char_for_sparkle(chosen_text, True)
             CW.putChar(sparkle, coord_x, coord_y)
 
-            bloody_pixels_on_screen[coord_x][coord_y] = sparkle
+            bloody_pixels_on_screen[coord_x][coord_y] = True
             total_shown_pixels += 1
-
         CW.flushConsole()
 
+    # SECOND: clear everything except the 'you're dead' label.
     total_shown_pixels = 0
     while total_shown_pixels < w * h:
         # draw blood
@@ -110,19 +128,16 @@ def show_death_screen(player):
             number = RND.rand(w * h)
             coord_x, coord_y = number % w, number // w
 
-            while bloody_pixels_on_screen[coord_x][coord_y] == ' ':
+            while not bloody_pixels_on_screen[coord_x][coord_y]:
                 number += 1
                 if number >= w * h:
                     number = 0
                 coord_x, coord_y = number % w, number // w
 
             CW.setForegroundColor(sparkles_color)
-            CW.putChar(' ', coord_x, coord_y)
-            bloody_pixels_on_screen[coord_x][coord_y] = ' '
+            CW.putChar(get_char_for_sparkle(chosen_text, False, coord_x, coord_y), coord_x, coord_y)
+            bloody_pixels_on_screen[coord_x][coord_y] = False
             total_shown_pixels += 1
-
-        # text_r = int(255 * total_shown_pixels / (w * h) )
         CW.setForegroundColor(sparkles_color)
-        _draw_death_text()
         CW.flushConsole()
     CW.readKey()
