97458a69a741e32ca5a260d4f0f4bdf859649bdb
sidav <sidav94@gmail.com>
2018-06-16 16:47:34 +0300

    WIP: adds stub death screen.

---
+++ Level_Routines/Player/DeathScreen.py
@@ -0,0 +1,33 @@
+from Routines import TdlConsoleWrapper as CW
+from GLOBAL_DATA import Global_Constants as GC
+
+_death_text = [
+    "                         YOU'RE DEAD                                ",
+    '                                                                    ',
+    'this is a placeholder death screen. More beautiful one coming soon. '
+]
+
+
+def _determine_x_position(arr):
+    ww = GC.CONSOLE_WIDTH
+    if isinstance(arr, list):
+        picw = len(arr[0])
+        return int((ww - picw) * 0.5)
+    elif isinstance(arr, str):
+        picw = len(arr)
+        return int((ww - picw)*0.47)
+
+
+def _determine_y_position(arr):
+    wh = GC.CONSOLE_HEIGHT
+    pich = len(arr)
+    return int((wh-pich)/3)
+
+
+def show_death_screen(player):
+    CW.setForegroundColor(196, 0, 0)
+    x = _determine_x_position(_death_text)
+    y = _determine_y_position(_death_text)
+    CW.drawCharArrayAtPosition(_death_text, x, y, True)
+    CW.flushConsole()
+    CW.readKey()
