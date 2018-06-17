94930b071f4d309378c7923ad8eb142e21d1cded
Не Скажу <sidav94@gmail.com>
2018-06-17 21:50:13 +0300

    WHO THE HELL ON EARTH could think that these variables are static?

--- Level_Routines/Mechanics/Sneak.py
+++ Level_Routines/Mechanics/Sneak.py
@@ -1,9 +1,9 @@
 class RpgStats:
-    _strength = 5
-    _nimbleness = 5
-    _endurance = 5
-    _advertence = 5
-    _knowledge = 5
+    # _strength = 5
+    # _nimbleness = 5
+    # _endurance = 5
+    # _advertence = 5
+    # _knowledge = 5
 
     def __init__(self, s=5, n=5, e=5, a=5, k=5):
         self._strength = s
