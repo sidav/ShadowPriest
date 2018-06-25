5b54496ae73f2e4ead31c8607a83a05d092a1d6d
Vladislav Kovun <vkovun@phoenixit.ru>
2018-06-25 09:24:05 +0300

    Fixes the status effects ending earlier than they should.

--- Level_Routines/Controllers/StatusEffectsController.py
+++ Level_Routines/Controllers/StatusEffectsController.py
@@ -4,11 +4,12 @@ from Message_Log import MessageLog as LOG
 def apply_status_effects_to_a_unit(unit):
     all_se = unit.get_status_effects()
     for status_effect in all_se:
-        if status_effect.get_expiration_turn() < LC.get_current_turn():
-            unit.remove_status_effect(status_effect)
-        elif status_effect.get_name() == 'HEALING':
+        if status_effect.get_name() == 'HEALING':
             unit.increase_hitpoints(1)
         elif status_effect.get_name() == 'POISON':
             unit.decrease_hitpoints(1)
+
+        if status_effect.get_expiration_turn() < LC.get_current_turn():
+            unit.remove_status_effect(status_effect)
         else:
             LOG.append_warning_message('Unknown status effect "{}" passed to a controller!'.format(status_effect.get_name()))
