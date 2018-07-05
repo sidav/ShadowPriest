c996ba23a577a9127ee88f1d1b7b31c738097fe9
Не Скажу <sidav94@gmail.com>
2018-07-06 00:17:56 +0300

    WIP: Attempt to make a exe build with cx_freeze and py2exe (no success yet).

---
+++ setup.py
@@ -0,0 +1,26 @@
+# TO USE CX_FREEZE:
+# type "python setup.py build" in terminal
+
+
+import sys
+from cx_Freeze import setup, Executable
+
+additional_mods = ['numpy.core._methods', 'numpy.lib.format', "_cffi_backend"]
+setup(
+    name = "Shadow Priest",
+    version = "0.2.0",
+    description = "A stealth-focused roguelike.",
+    executables = [Executable("ShadowPriest.py")]
+)
+
+# NEXT LINES ARE FOR py2exe!
+# type "python setup.py py2exe" in terminal
+
+# from distutils.core import setup
+# import py2exe
+# options={
+#     "py2exe": {
+#         "includes": "tdl"
+#     }
+# }
+# setup(console=['ShadowPriest.py'])
