116bc0a57408a05aa16e502a7308400f9a89af06
Unknown <sidav94@gmail.com>
2018-07-25 19:09:39 +0300

    Fixes the wrong damage statistics.

--- Level_Routines/Mechanics/Damage.py
+++ Level_Routines/Mechanics/Damage.py
@@ -3,7 +3,9 @@ from Message_Log import MessageLog as LOG
 from ..Player import Statistics as stat
 
 def do_damage_to_victim(damage, victim):
+    victim.decrease_hitpoints(damage)
     if victim.get_current_hitpoints() <= 0 and not victim.is_of_type('Player'):
         stat.enemies_killed += 1
-    victim.decrease_hitpoints(damage)
+    elif victim.is_of_type('Player'):
+        stat.total_hp_lost += damage
     LOG.append_warning_message('{} damage, {}\'s HP: {}/{}'.format(damage, victim.get_name(), victim.get_current_hitpoints(), victim.get_max_hitpoints()))
