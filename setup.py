115dd42c0b3749c820f1698e0e339b636a0534d3
Unknown <sidav94@gmail.com>
2018-07-25 18:39:52 +0300

    Even more improves the build routine.

--- setup.py
+++ setup.py
@@ -6,14 +6,17 @@ import sys
 from cx_Freeze import setup, Executable
 
 additional_mods = ['numpy.core._methods', 'numpy.lib.format', "_cffi_backend"]
-excluded_mods = ['scipy']#["concurrent", "ctypes", "curses", "dateutil", "distutils", "email", "html", "http",
+excluded_mods = ['scipy', 'http', 'html', 'email']#["concurrent", "ctypes", "curses", "dateutil", "distutils",
                  # "importlib", "json", "lib2to3", "matplotlib", "multiprocessing", "pkg_resources", "pycparser",
                  # "pydoc_data", "pytz", "scipy", "setuptools", "urllib", "xml", "xmlrpc"]
+include_files = ['shadowpriest8x12_gs_ro.png', 'shadowpriest16x24_gs_ro.png', 'shadowpriest12x20.png']
+
 setup(
     name = "Shadow Priest",
     version = "0.2.0",
     description = "A stealth-focused roguelike.",
-    options = {'build_exe': {'build_exe': 'build/ShadowPriest', 'includes': additional_mods, 'excludes': excluded_mods, 'optimize': 2}},
+    options = {'build_exe': {'build_exe': 'build/ShadowPriest', 'includes': additional_mods, 'excludes': excluded_mods,
+                             'include_files': include_files, 'optimize': 2}},
     executables = [Executable("ShadowPriest.py")]
 )
 
