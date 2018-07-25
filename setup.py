1b796257bf84566e8e7d28afda16b885e086e124
Unknown <sidav94@gmail.com>
2018-07-25 16:38:49 +0300

    Updates the setup.

--- setup.py
+++ setup.py
@@ -6,11 +6,14 @@ import sys
 from cx_Freeze import setup, Executable
 
 additional_mods = ['numpy.core._methods', 'numpy.lib.format', "_cffi_backend"]
+excluded_mods = ['scipy']#["concurrent", "ctypes", "curses", "dateutil", "distutils", "email", "html", "http",
+                 # "importlib", "json", "lib2to3", "matplotlib", "multiprocessing", "pkg_resources", "pycparser",
+                 # "pydoc_data", "pytz", "scipy", "setuptools", "urllib", "xml", "xmlrpc"]
 setup(
     name = "Shadow Priest",
     version = "0.2.0",
     description = "A stealth-focused roguelike.",
-    options = {'build_exe': {'includes': additional_mods}},
+    options = {'build_exe': {'includes': additional_mods, 'excludes': excluded_mods, 'optimize': 2}},
     executables = [Executable("ShadowPriest.py")]
 )
 
